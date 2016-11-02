1f6db6598ab0c1d8769a3d827d5038b3e44c2863
pixelhijack <pothattila@gmail.com>
2016-11-02 20:25:02 +0000

    [BOUNDTO] full rectangle

--- client/dist/bundle.js
+++ client/dist/bundle.js
@@ -1152,6 +1152,14 @@
 	                this.boundTo = new Phaser.Rectangle(bounds.x1, 0, bounds.x2 - bounds.x1, this.game.height);
 	        }
 	        
+	        // {x1, y1, x2, y2}
+	        if(bounds.hasOwnProperty('x1') && 
+	            bounds.hasOwnProperty('x2') &&
+	            bounds.hasOwnProperty('y1') &&
+	            bounds.hasOwnProperty('y2')){
+	                this._boundTo = new Phaser.Rectangle(bounds.x1, bounds.y1, bounds.x2 - bounds.x1, bounds.y2 - bounds.y1);
+	        }
+	        
 	        return this;
 	    },
 	    checkBounds: function(){
